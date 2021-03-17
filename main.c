ef610c515fa28fb08ee2daafdde21a77374fd81e
Seth Barberee <seth.barberee@gmail.com>
2021-03-16 19:34:21 -0500

    rename sample to algae. thx wlroots for good examples

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #include <wlr/util/log.h>
 #include <xkbcommon/xkbcommon.h>
 
-struct sample_state {
+struct algae_state {
 	struct wl_display *display;
 	struct wl_listener new_output;
 	struct wl_listener new_input;
@@ -23,40 +23,40 @@ struct sample_state {
 	int dec;
 };
 
-struct sample_output {
-	struct sample_state *sample;
+struct algae_output {
+	struct algae_state *algae;
 	struct wlr_output *output;
 	struct wl_listener frame;
 	struct wl_listener destroy;
 };
 
-struct sample_keyboard {
-	struct sample_state *sample;
+struct algae_keyboard {
+	struct algae_state *algae;
 	struct wlr_input_device *device;
 	struct wl_listener key;
 	struct wl_listener destroy;
 };
 
 static void output_frame_notify(struct wl_listener *listener, void *data) {
-	struct sample_output *sample_output =
-		wl_container_of(listener, sample_output, frame);
-	struct sample_state *sample = sample_output->sample;
-	struct wlr_output *wlr_output = sample_output->output;
+	struct algae_output *algae_output =
+		wl_container_of(listener, algae_output, frame);
+	struct algae_state *algae = algae_output->algae;
+	struct wlr_output *wlr_output = algae_output->output;
 
 	struct timespec now;
 	clock_gettime(CLOCK_MONOTONIC, &now);
 
-	long ms = (now.tv_sec - sample->last_frame.tv_sec) * 1000 +
-		(now.tv_nsec - sample->last_frame.tv_nsec) / 1000000;
-	int inc = (sample->dec + 1) % 3;
+	long ms = (now.tv_sec - algae->last_frame.tv_sec) * 1000 +
+		(now.tv_nsec - algae->last_frame.tv_nsec) / 1000000;
+	int inc = (algae->dec + 1) % 3;
 
-	sample->color[inc] += ms / 2000.0f;
-	sample->color[sample->dec] -= ms / 2000.0f;
+	algae->color[inc] += ms / 2000.0f;
+	algae->color[algae->dec] -= ms / 2000.0f;
 
-	if (sample->color[sample->dec] < 0.0f) {
-		sample->color[inc] = 1.0f;
-		sample->color[sample->dec] = 0.0f;
-		sample->dec = inc;
+	if (algae->color[algae->dec] < 0.0f) {
+		algae->color[inc] = 1.0f;
+		algae->color[algae->dec] = 0.0f;
+		algae->dec = inc;
 	}
 
 	wlr_output_attach_render(wlr_output, NULL);
@@ -64,46 +64,46 @@ static void output_frame_notify(struct wl_listener *listener, void *data) {
 	struct wlr_renderer *renderer =
 		wlr_backend_get_renderer(wlr_output->backend);
 	wlr_renderer_begin(renderer, wlr_output->width, wlr_output->height);
-	wlr_renderer_clear(renderer, sample->color);
+	wlr_renderer_clear(renderer, algae->color);
 	wlr_renderer_end(renderer);
 
 	wlr_output_commit(wlr_output);
-	sample->last_frame = now;
+	algae->last_frame = now;
 }
 
 static void output_remove_notify(struct wl_listener *listener, void *data) {
-	struct sample_output *sample_output =
-		wl_container_of(listener, sample_output, destroy);
+	struct algae_output *algae_output =
+		wl_container_of(listener, algae_output, destroy);
 	wlr_log(WLR_DEBUG, "Output removed");
-	wl_list_remove(&sample_output->frame.link);
-	wl_list_remove(&sample_output->destroy.link);
-	free(sample_output);
+	wl_list_remove(&algae_output->frame.link);
+	wl_list_remove(&algae_output->destroy.link);
+	free(algae_output);
 }
 
 static void new_output_notify(struct wl_listener *listener, void *data) {
 	struct wlr_output *output = data;
-	struct sample_state *sample =
-		wl_container_of(listener, sample, new_output);
-	struct sample_output *sample_output =
-		calloc(1, sizeof(struct sample_output));
+	struct algae_state *algae =
+		wl_container_of(listener, algae, new_output);
+	struct algae_output *algae_output =
+		calloc(1, sizeof(struct algae_output));
 
 	struct wlr_output_mode *mode = wlr_output_preferred_mode(output);
 	if (mode != NULL) {
 		wlr_output_set_mode(output, mode);
 	}
-	sample_output->output = output;
-	sample_output->sample = sample;
-	wl_signal_add(&output->events.frame, &sample_output->frame);
-	sample_output->frame.notify = output_frame_notify;
-	wl_signal_add(&output->events.destroy, &sample_output->destroy);
-	sample_output->destroy.notify = output_remove_notify;
+	algae_output->output = output;
+	algae_output->algae = algae;
+	wl_signal_add(&output->events.frame, &algae_output->frame);
+	algae_output->frame.notify = output_frame_notify;
+	wl_signal_add(&output->events.destroy, &algae_output->destroy);
+	algae_output->destroy.notify = output_remove_notify;
 
-	wlr_output_commit(sample_output->output);
+	wlr_output_commit(algae_output->output);
 }
 
 static void keyboard_key_notify(struct wl_listener *listener, void *data) {
-	struct sample_keyboard *keyboard = wl_container_of(listener, keyboard, key);
-	struct sample_state *sample = keyboard->sample;
+	struct algae_keyboard *keyboard = wl_container_of(listener, keyboard, key);
+	struct algae_state *algae = keyboard->algae;
 	struct wlr_event_keyboard_key *event = data;
 	uint32_t keycode = event->keycode + 8;
 	const xkb_keysym_t *syms;
@@ -112,13 +112,13 @@ static void keyboard_key_notify(struct wl_listener *listener, void *data) {
 	for (int i = 0; i < nsyms; i++) {
 		xkb_keysym_t sym = syms[i];
 		if (sym == XKB_KEY_Escape) {
-			wl_display_terminate(sample->display);
+			wl_display_terminate(algae->display);
 		}
 	}
 }
 
 static void keyboard_destroy_notify(struct wl_listener *listener, void *data) {
-	struct sample_keyboard *keyboard =
+	struct algae_keyboard *keyboard =
 		wl_container_of(listener, keyboard, destroy);
 	wl_list_remove(&keyboard->destroy.link);
 	wl_list_remove(&keyboard->key.link);
@@ -127,13 +127,13 @@ static void keyboard_destroy_notify(struct wl_listener *listener, void *data) {
 
 static void new_input_notify(struct wl_listener *listener, void *data) {
 	struct wlr_input_device *device = data;
-	struct sample_state *sample = wl_container_of(listener, sample, new_input);
+	struct algae_state *algae = wl_container_of(listener, algae, new_input);
 	switch (device->type) {
 	case WLR_INPUT_DEVICE_KEYBOARD:;
-		struct sample_keyboard *keyboard =
-			calloc(1, sizeof(struct sample_keyboard));
+		struct algae_keyboard *keyboard =
+			calloc(1, sizeof(struct algae_keyboard));
 		keyboard->device = device;
-		keyboard->sample = sample;
+		keyboard->algae = algae;
 		wl_signal_add(&device->events.destroy, &keyboard->destroy);
 		keyboard->destroy.notify = keyboard_destroy_notify;
 		wl_signal_add(&device->keyboard->events.key, &keyboard->key);
@@ -167,7 +167,7 @@ static void new_input_notify(struct wl_listener *listener, void *data) {
 int main(void) {
 	wlr_log_init(WLR_DEBUG, NULL);
 	struct wl_display *display = wl_display_create();
-	struct sample_state state = {
+	struct algae_state state = {
 		.color = { 1.0, 0.0, 0.0, 1.0 },
 		.dec = 0,
 		.last_frame = { 0 },
